174ffa1107ccbf56361e11dbf55f5ad99afe91be
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-10-25 23:31:20 +0630

    show table outside of form

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,31 +12,35 @@
 </script>
 
 <style>
-	.main {
+	.main, form {
 		margin: 1rem;
+		display: flex;
+		flex-flow: wrap;
+		justify-content: space-evenly;
 	}
+
 </style>
 
 <Header title="Currency loss"/>
 <main class="main">
 	{#if currencies}
 		<form>
-		<SelectCurrencies {currencies} bind:selected={selectedFrom}/>
-		<SelectCurrencies {currencies} bind:selected={selectedTo}/>
-		<input type="number" min="0" placeholder="Rate offered" bind:value={rateOffered}/>
-		{#if selectedFrom && selectedFrom !== selectedTo}
-			<FetchRates baseCurrency={selectedFrom} targetCurrencies={Object.keys(currencies)}
-				let:rates>
-				<input type="number" bind:value={amountToChange} placeholder="Amount to change" min="0"/>
-				<LossCalculations 
-					rateExpected={rates[selectedTo]}
-					rateOffered={rateOffered}
-					amountToChange={amountToChange}
-					targetCurrency={selectedTo}
-					baseCurrency={selectedFrom}
-					/>
-			</FetchRates>
-		{/if}
+			<SelectCurrencies {currencies} bind:selected={selectedFrom}/>
+			<SelectCurrencies {currencies} bind:selected={selectedTo}/>
+			<input type="number" min="0" placeholder="Rate offered" bind:value={rateOffered}/>
+			<input type="number" bind:value={amountToChange} placeholder="Amount to change" min="0"/>
 		</form>
+	{/if}
+	{#if selectedFrom && selectedFrom !== selectedTo}
+		<FetchRates baseCurrency={selectedFrom} targetCurrencies={Object.keys(currencies)}
+			let:rates>
+			<LossCalculations 
+				rateExpected={rates[selectedTo]}
+				rateOffered={rateOffered}
+				amountToChange={amountToChange}
+				targetCurrency={selectedTo}
+				baseCurrency={selectedFrom}
+				/>
+		</FetchRates>
 	{/if}
 </main>
