49b1375c1a49c3d3782b785d9cf592009fd2747d
Carlos Martin Sanchez <carlosvin@gmail.com>
2019-10-26 21:04:43 +0630

    improve styling

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -12,35 +12,43 @@
 </script>
 
 <style>
-	.main, form {
+	.main {
+		margin: 1rem;
+		display: flex;
+		flex-flow: column;
+		justify-content: space-around;
+	}
+	form {
 		margin: 1rem;
 		display: flex;
 		flex-flow: wrap;
 		justify-content: space-evenly;
+		justify-items: center;
 	}
-
+	.calculatedTable {
+		display: flex;
+		justify-content: center;
+	}
 </style>
 
 <Header title="Currency loss"/>
 <main class="main">
-	{#if currencies}
-		<form>
-			<SelectCurrencies {currencies} bind:selected={selectedFrom}/>
-			<SelectCurrencies {currencies} bind:selected={selectedTo}/>
-			<input type="number" min="0" placeholder="Rate offered" bind:value={rateOffered}/>
-			<input type="number" bind:value={amountToChange} placeholder="Amount to change" min="0"/>
-		</form>
-	{/if}
+	<form>
+		<SelectCurrencies {currencies} bind:selected={selectedFrom}/>
+		<SelectCurrencies {currencies} bind:selected={selectedTo}/>
+		<input type="number" min="0" bind:value={rateOffered} placeholder="Rate offered" />
+		<input type="number" min="0" bind:value={amountToChange} placeholder="Amount to change"/>
+	</form>
 	{#if selectedFrom && selectedFrom !== selectedTo}
-		<FetchRates baseCurrency={selectedFrom} targetCurrencies={Object.keys(currencies)}
-			let:rates>
-			<LossCalculations 
-				rateExpected={rates[selectedTo]}
-				rateOffered={rateOffered}
-				amountToChange={amountToChange}
-				targetCurrency={selectedTo}
-				baseCurrency={selectedFrom}
-				/>
-		</FetchRates>
+		<div class='calculatedTable'>
+			<FetchRates baseCurrency={selectedFrom} targetCurrencies={Object.keys(currencies)} let:rates>
+				<LossCalculations 
+					rateExpected={rates[selectedTo]}
+					rateOffered={rateOffered}
+					amountToChange={amountToChange}
+					targetCurrency={selectedTo}
+					baseCurrency={selectedFrom} />
+			</FetchRates>
+		</div>
 	{/if}
 </main>
